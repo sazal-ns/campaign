c581e5f97964873ff0f4100e3225a12c27861754
Sazal Siddiqui NS <sazal.siddiqui@gmail.com>
2017-03-11 16:55:10 +0600

    Splash Done

--- app/src/main/java/com/rtsoftbd/siddiqui/campaign/SplashActivity.java
+++ app/src/main/java/com/rtsoftbd/siddiqui/campaign/SplashActivity.java
@@ -1,15 +1,58 @@
 package com.rtsoftbd.siddiqui.campaign;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
 
 public class SplashActivity extends AppCompatActivity {
 
+    final static private String PREF_KEY_SHORTCUT_ADDED = "PREF_KEY_SHORTCUT_ADDED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        addShortCut();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+
+
+
+
     }
+
+    private void addShortCut() {
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        boolean shortCutWasAlreadyAdded = sharedPreferences.getBoolean(PREF_KEY_SHORTCUT_ADDED, false);
+        if (shortCutWasAlreadyAdded) return;
+
+        Intent shortCut = new Intent(getApplicationContext(), SplashActivity.class);
+        shortCut.setAction(Intent.ACTION_MAIN);
+
+        Intent addIntent = new Intent();
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortCut);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME,getResources().getString(R.string.app_name));
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(getApplicationContext(),R.drawable.round_icon));
+        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        addIntent.putExtra("duplicate", false);
+        getApplicationContext().sendBroadcast(addIntent);
+
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(PREF_KEY_SHORTCUT_ADDED, true);
+        editor.apply();
+
+    }
+
 }
