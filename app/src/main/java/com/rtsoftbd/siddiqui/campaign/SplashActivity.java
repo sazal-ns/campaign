a00d2393737a76cf865368e55145887a568e736a
Sazal Siddiqui NS <sazal.siddiqui@gmail.com>
2017-03-11 11:24:55 +0600

    add to git

---
+++ app/src/main/java/com/rtsoftbd/siddiqui/campaign/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.rtsoftbd.siddiqui.campaign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
